3867c57b8a542c75a012e3cefb00313ea8fd5839
Sofiya Semenova <ssemenova@edx.org>
2018-05-03 15:08:48 -0400

    Commenting

--- openedx/core/djangoapps/user_api/management/commands/create_user_gdpr_testing.py
+++ openedx/core/djangoapps/user_api/management/commands/create_user_gdpr_testing.py
@@ -31,6 +31,13 @@ class Command(BaseCommand):
             required=False,
             help='Username'
         )
+        parser.add_argument(
+            '-e', '--email',
+            nargs=1,
+            required=False,
+            help='Email'
+        )
+
 
     def handle(self, *args, **options):
         """
@@ -38,12 +45,14 @@ class Command(BaseCommand):
         """
 
         username = options['username'] if options['username'] else 'gdpr_test_user'
+        email = options['email'] if options['email'] else 'gdpr_test_user@example.com'
+
         user = User.objects.create(
             user=username,
-            email='gdpr_test_user@example.com'
+            email=email
         )
 
-        # clear_pii_from_userprofile
+        # UserProfile
         profile_image_uploaded_date = datetime.datetime(2018, 5, 3, tzinfo=UTC)
         UserProfile.objects.create(
             user=user,
@@ -62,15 +71,15 @@ class Command(BaseCommand):
             is_active=True,
         )
 
-        # delete_users_profile_images
+        # Profile images
         # Need a way to upload a profile image?
 
-        # retire_users_data_sharing_consent
+        # DataSharingConsent
         DataSharingConsent.objects.create(
             username=username,
         )
 
-        # retire_sapsf_data_transmission
+        # Sapsf data transmission
         enterprise_user = EnterpriseCustomerUser.objects.create(
             user_id=user.id
         )
@@ -81,12 +90,12 @@ class Command(BaseCommand):
             enterprise_course_enrollment_id=audit.id
         )
 
-        # retire_user_from_pending_enterprise_customer_user
+        # PendingEnterpriseCustomerUser
         PendingEnterpriseCustomerUser.objects.create(
             user_email=user.email
         )
 
-        # retire_entitlement_support_detail
+        # EntitlementSupportDetail
         CourseEntitlement.objects.create(
             user_id=user.id,
 
@@ -96,7 +105,7 @@ class Command(BaseCommand):
             comments='bla bla test comments bla bla'
         )
 
-        # Retire misc. models that may contain PII of this user
+        # Misc. models that may contain PII of this user
         SoftwareSecurePhotoVerification.objects.create(
             user=user,
             name='gdpr test',
@@ -111,7 +120,7 @@ class Command(BaseCommand):
             email=user.email
         )
 
-        # Retire any objects linked to the user via their original email
+        # Objects linked to the user via their original email
         CourseEnrollmentAllowed.objects.create(
             email=user.email
         )
